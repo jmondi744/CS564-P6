3f3aec71f4c3f96f6a725f88082284497d6cd735
jmondi744 <102432792+jmondi744@users.noreply.github.com>
2022-12-11 22:22:22 -0600

    Added start of delete

--- delete.C
+++ delete.C
@@ -16,8 +16,59 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
+  // part 6
+  
+  Status status;
+  if(status != OK) {
+      return status;
+  }
+  int count = 0; 
+  RID rid;
+  
+  HeapFileScan relScan(relation, status);
+  
+  //Null attrName check
+  if(attrName.length() == 0) {
+      
+  }
+  
+  //Check for type
+  char* holder;
+  switch(type) {
+      case STRING
+          holder = attrValue;
+          break;
+      
+      case INTEGER
+          holder = (char*)&(atoi(attrValue));
+          break;
+      
+      case FLOAT
+          holder = (char*)&(atof(attrValue));
+          break;          
+  }
+  
+  AttrDesc desc;
+  status = attrCat->getInfo(relation, attrName, attrDesc);
+  if(status != OK) {
+      return status;
+  }
+  
+  //Scan for tuples that match
+  status = relScan.startSCan(arrtDesc.attrOffset, attrDesc.attrLen, type, holder,op);
+  if(status != OK) {
+      return status;
+  }
+  
+  while(relScan.scanNext(rid) == OK) {
+      status = relScan.deleteRecodr();
+      if(status != OK) {
+        return status;
+      }    
+      count++;
+  }   
+  
+  return OK;
 
 
 
