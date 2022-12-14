3ae248dc5de63b3bae5428387b1d36bafac6e982
jmondi744 <102432792+jmondi744@users.noreply.github.com>
2022-12-14 11:48:21 -0600

    Edited Files All Together

--- delete.C
+++ delete.C
@@ -16,11 +16,64 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
-
-
-
+  // part 6
+  
+  Status status;
+  int count = 0; 
+  RID rid;
+  
+  HeapFileScan relScan(relation, status);
+  
+  //Null attrName check
+  if(attrName.length() == 0) {  //not sure about length check
+      //set offset to 0
+      //set length to 0
+      //set type to string
+      //set filter to null
+      status = relScan.startScan(0, 0, STRING, NULL, EQ);    
+  } else {
+      //Check for type
+      const char* holder;
+      int holder2;
+      float holder3;
+      
+      switch(type) {
+          case STRING:
+              holder = attrValue;
+              break;
+      
+          case INTEGER:
+              holder2 = atoi(attrValue);
+              holder = (char*)&(holder2);
+              break;
+        
+          case FLOAT:
+              holder3 = atof(attrValue);
+              holder = (char*)&(holder3);
+              break;          
+      }
+  
+      AttrDesc desc;
+      status = attrCat->getInfo(relation, attrName, desc);
+      if(status != OK) {
+          return status;
+      }
+      status = relScan.startScan(desc.attrOffset, desc.attrLen, type, holder,op);
+  }
+  
+  if(status != OK) {
+      return status;
+  } 
+  
+  while(relScan.scanNext(rid) == OK) {
+      status = relScan.deleteRecord();
+      if(status != OK) {
+          return status;
+      }    
+      count++;
+  }   
+  
+  return OK;
 }
 
 
--- insert.C
+++ insert.C
@@ -14,8 +14,53 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
+
+	Status status;
+	Record record;
+	int catattrCnt;
+	AttrDesc * attrs;
+	status = attrCat->getRelInfo(relation, catattrCnt, attrs);
+	if(status) {
+		return status;
+	}
+	int length = 0;
+	for(int i = 0; i < catattrCnt; i++) {
+		length += attrs[i].attrLen;
+	}
+	record.length = length;
+	char data[length];
+	record.data = (void *) data;
+	for(int i = 0; i < attrCnt; i++) {
+		for(int y = 0; y < catattrCnt; y++) {
+			if(!strcmp(attrList[i].attrName, attrs[y].attrName)) {
+				if(attrList[i].attrValue == NULL) {
+					return BADCATPARM;
+				} else {
+					if(attrList[i].attrType == INTEGER) {
+						int a = atoi((char *) attrList[i].attrValue);
+						memcpy(data+ attrs[y].attrOffset, (char *)&a, attrs[y].attrLen);
+					} else if(attrList[i].attrType == FLOAT) {
+						float a = atof((char *) attrList[i].attrValue);
+						memcpy(data + attrs[y].attrOffset, (char *)&a, attrs[y].attrLen);
+					} else if(attrList[i].attrType == FLOAT) {
+						memcpy(data + attrs[y].attrOffset, (char *) attrList[i].attrValue, attrs[y].attrLen);
+					} else {
+						return ATTRTYPEMISMATCH;
+					}
+				}
+			}
+		}
+	}
+	RID rid;
+	InsertFileScan ifs(relation, status);
+	if(status) {
+		return status;
+	}
+	status = ifs.insertRecord(record, rid);
+	if(status) {
+		return status;
+	}
 // part 6
-return OK;
+	return OK;
 
 }
-
